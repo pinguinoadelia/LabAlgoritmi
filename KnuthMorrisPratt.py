d0af34cea0dff14c09e4037f80305ffab1027442
pinguinoadelia <gianmarco.delaurentiis@stud.unifi.it>
2024-12-06 13:36:15 +0100

    creazione funzione compute_prefix_function

--- KnuthMorrisPratt.py
+++ KnuthMorrisPratt.py
@@ -2,7 +2,26 @@
 class KnuthMorrisPratt:
 
     def __init__(self, pattern):
-        this.pattern = pattern
+        this.pattern = pattern    
+
+    def compute_prefix_function(self, pattern):
+        m = len(pattern)
+        pi = [0] * m
+        k = 0
+        for q in range (1,m):
+            while k > 0 and pattern[k] != pattern[q]:
+                k = pi[k - 1]
+            if pattern[k] == pattern[q]:
+                k += 1
+            pi[q] = k
+        return pi
+    
+          
+
+
+
+
+
 
 
         
