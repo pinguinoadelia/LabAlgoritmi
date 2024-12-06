57ef90b19cba178181cc32a691899582133c79c1
pinguinoadelia <gianmarco.delaurentiis@stud.unifi.it>
2024-12-06 12:01:24 +0100

    Creazione funzione find_pattern

--- naive_string_matching.py
+++ naive_string_matching.py
@@ -5,4 +5,13 @@ class NaiveStringMatching:
         self.text = text
         self.pattern = pattern
 
+    def find_pattern(self) -> list:
+        n = len(self.text)
+        m = len(self.pattern)
+        results = []
+        for s in range (n - m + 1):
+            if self.text[s:s + m] == self.pattern:
+                results.append(s)
+        return results
+
     
