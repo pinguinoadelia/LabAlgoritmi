f7b2059849a76c2506031bd5df6d87dcafe72d0b
pinguinoadelia <gianmarco.delaurentiis@stud.unifi.it>
2024-12-07 15:49:56 +0100

    Creazione funzione run_experiments

--- experiment_runner.py
+++ experiment_runner.py
@@ -23,4 +23,40 @@ class ExperimentRunner:
     def generate_random_string(self,length, charset = "abcdefghijklmnopqrstuvwxyz"):
         return ''.join(random.choice(charset) for _ in range(length))
     
+    def run_experiments(self):
+        results = []
+
+        # Test 1: Testo casuale e pattern casuale con dimensioni crescenti
+        for size in [100, 1000, 10000]:
+            text = self.generate_random_string(size)
+            pattern = self.generate_random_string(size // 2)
+            naive_time = self.execute_naive(text, pattern)
+            kmp_time = self.execute_kmp(text, pattern)
+            results.append({'test': 'Testo casuale e Pattern casuale', 'size': size, 'naive_time': naive_time, 'kmp_time': kmp_time})
+
+        # Test 2: Testo lungo e pattern breve
+        for size in [1000, 10000, 50000]:
+            text = self.generate_random_string(size)
+            pattern = self.generate_random_string(5)
+            naive_time = self.execute_naive(text, pattern)
+            kmp_time = self.execute_kmp(text, pattern)
+            results.append({'test': 'Testo lungo e Pattern breve', 'size': size, 'naive_time': naive_time, 'kmp_time': kmp_time})
+
+        # Test 3: Testo ripetitivo con un pattern lungo
+        for size in [100, 1000, 10000]:
+            text = "a" * size
+            pattern = "a" * (size // 2)
+            naive_time = self.execute_naive(text, pattern)
+            kmp_time = self.execute_kmp(text, pattern)
+            results.append({'test': 'Testo ripetitivo e Pattern lungo', 'size': size, 'naive_time': naive_time, 'kmp_time': kmp_time})
+
+        # Test 4: Pattern non presente nel testo
+        for size in [100, 1000, 10000]:
+            text = self.generate_random_string(size, charset="abc")
+            pattern = self.generate_random_string(10, charset="xyz")
+            naive_time = self.execute_naive(text, pattern)
+            kmp_time = self.execute_kmp(text, pattern)
+            results.append({'test': 'Pattern non presente', 'size': size, 'naive_time': naive_time, 'kmp_time': kmp_time})
+        return results
+
     
