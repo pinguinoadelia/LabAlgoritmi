eca0ca48224542d769c07ead0832e1fca5013488
pinguinoadelia <gianmarco.delaurentiis@stud.unifi.it>
2024-12-07 15:40:45 +0100

    Creazione funzione execute_naive e importazione di time e random

--- experiment_runner.py
+++ experiment_runner.py
@@ -1,5 +1,15 @@
+import time
+import random
 
 class ExperimentRunner:
 
     def __init__(self):
         pass
+
+    def execute_naive(self, text, pattern):
+        matcher = NaiveStringMatching(text, pattern)
+        start_time = time.time()
+        result = matcher.find_pattern()
+        elapsed_time = time.time() - start_time
+        return elapsed_time
+    
